8f0edc7d6391054b6e0cf952e5e8dea70b43684d
Esteban Orosco <eorosco360@gmail.com>
2023-01-16 23:36:32 -0500

    actualizando funcion lambda

--- funcionLambda.py
+++ funcionLambda.py
@@ -5,10 +5,9 @@ import mercadopago
 
 def lambda_handler(event, context):
     sdk = mercadopago.SDK(os.environ["TEST_TOKEN"])
-    bodyGet = json.loads(event["body"])
-    # TODO implement
+    items = event["items"]
     preference_data = {
-       "items": bodyGet["items"]
+       "items": items
     }
 
     preference_response = sdk.preference().create(preference_data)
@@ -16,12 +15,6 @@ def lambda_handler(event, context):
     print(json.dumps(preference))
     return {
         "statusCode": 200,
-        'headers': {
-            'Access-Control-Expose-Headers': 'Content-Type',
-            'Access-Control-Allow-Origin': '*',
-            'Access-Control-Allow-Methods': 'OPTIONS,POST,GET'
-        },
-
         "body": json.dumps(
             preference
         ),
